e3357bf28c5a764ac5f84c59c86f532ac6b74c20
Jesse <Jesse@Jesses-Mac-mini.attlocal.net>
2023-06-02 16:11:26 -0700

    client side connected to backend, CRUD operations work, basic bootstrap is applied

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+// Import react-boostrap
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Nav from 'react-bootstrap/Nav'
+
+// Import components
+import Modal from './components/Modal'
 
 function App() {
+  const [viewCompleted, setViewCompleted] = useState(false);
+  const [todoList, setTodoList] = useState([])
+  const [modal, setModal] = useState(false)
+  const [activeItem, setActiveItem] = useState({
+    title: "",
+    description: "",
+    completed: false,
+  })
+
+  useEffect(() => {
+    refreshList()
+  }, [])
+
+  const refreshList = () => {
+    axios.get("/api/todos/").then((res) => setTodoList(res.data)).catch((err) => console.log(err));
+  }
+  const toggle = () => {
+    setModal(!modal)
+  }
+  const handleSubmit = (item) => {
+    console.log({...item});
+    toggle()
+    if (item.id) {
+      axios.put(`/api/todos/${item.id}/`, {...item}).then((res) => refreshList())
+      return
+    }
+    axios.post(`/api/todos/`, {...item}).then((res) => refreshList())
+  }
+  const handleDelete = (item) => {
+    axios.delete(`/api/todos/${item}/`).then((res) => refreshList())
+  }
+  const createItem = () => {
+    const item = { title: '', description: '', completed: false}
+    setActiveItem(item)
+    setModal(!modal)
+  }
+
+  const editItem = (item) => {
+    setActiveItem(item)
+    setModal(!modal)
+  }
+  const displayCompleted = (status) => {
+    setViewCompleted(status)
+  }
+  const renderTabList = () => {
+    return (
+      <Nav variant="tabs">
+        <Nav.Item>
+          <Nav.Link active={viewCompleted} onClick={() => displayCompleted(true)}>
+            Complete
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link active={viewCompleted} onClick={() => displayCompleted(false)}>
+            Incomplete
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    )
+  }
+
+  const renderItems = () => {
+    const newItems = todoList.filter(
+      (item) => item.completed === viewCompleted
+    )
+    return newItems.map((item) => (
+      <li key={item.id} className='list-group-item d-flex justify-content-between align-items-center'>
+        <span className={`todo-title mr-2 ${viewCompleted ? "completed-todo" : ""}`} title={item.description}>
+          {item.title}
+        </span>
+        <span>
+          <Button variant='secondary' className='mr-2' onClick={() => editItem(item)}>Edit</Button>
+          <Button variant='danger' onClick={(e) => handleDelete(item.id)}>Delete</Button>
+        </span>
+      </li>
+    ))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <h1 className='text-uppercase text-center my-4'>Todo app</h1>
+      <Row>
+        <Col md={6} sm={10} className='mx-auto p-0'>
+          <Card className='p-3'>
+            <div className='mb-4'>
+              <Button variant='primary' onClick={createItem}>Add Todo</Button>
+            </div>
+              {renderTabList()}
+              <ul className='list-group list-group-flush border-top-0'>
+                {renderItems()}
+              </ul>
+            
+          </Card>
+        </Col>
+      </Row>
+      {modal && (
+        <Modal show={modal} onHide={toggle} activeItem={activeItem} onSave={handleSubmit} />
+      )}
+    </Container>
+  )
+
+
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+//Import react-bootstrap
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+
+
+const CustomModal = ({ activeItem, toggle, onSave }) => {
+    const [item, setItem] = useState(activeItem)
+
+    const handleChange = (e) => {
+        const {name, value, type, checked } = e.target
+        const newValue = type === 'checkbox' ? checked : value;
+
+        setItem({...item, [name]: newValue})
+    }
+
+    return (
+        <>
+          <Modal show={true} onHide={toggle}>
+            <Modal.Header closeButton>
+                <Modal.Title>Todo Item</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type='text' name='title' value={item.title} onChange={handleChange} placeholder="Enter Todo Title"/>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control type='text' name='description' value={item.description} onChange={handleChange} placeholder="Enter Todo description" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Check type='checkbox' name='completed' checked={item.completed} label='Completed' onChange={handleChange} /> 
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant='success' onClick={() => onSave(item)}>Save</Button>
+            </Modal.Footer>
+          </Modal>
+        </>
+    )
+}
+
+export default CustomModal
