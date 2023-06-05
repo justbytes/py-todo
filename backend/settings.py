ade6a7fc231fcdc3d971f6bd7078380aaef9fca5
Jesse <Jesse@Jesses-Mac-mini.attlocal.net>
2023-06-05 16:52:25 -0700

    fixing typo

--- backend/settings.py
+++ backend/settings.py
@@ -130,8 +130,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATIC_DIRS = [os.path.join(BASE_DIR, "build/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "build/static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
