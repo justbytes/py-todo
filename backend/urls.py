322fa84c213acfeea86ff25bc1b35be7ce851468
Jesse <Jesse@Jesses-Mac-mini.attlocal.net>
2023-06-04 23:46:51 -0700

    adding static files dir

--- backend/urls.py
+++ backend/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from rest_framework.routers import DefaultRouter
 from todo.views import TodoViewSet
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r"todos", TodoViewSet, basename="todo")
@@ -27,4 +28,5 @@ router.register(r"todos", TodoViewSet, basename="todo")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    re_path(".*", TemplateView.as_view(template_name="index.html")),
 ]
